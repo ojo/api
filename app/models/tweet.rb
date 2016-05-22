b0c69bd9a846af82575b7c7e5f3ea3157bbdabe8
Brian Tiger Chow <brian.holderchow@gmail.com>
2016-05-22 09:50:01 -0700

    define url

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,4 +2,8 @@ class Tweet < ApplicationRecord
   belongs_to :managed_twitter_account
   validates_presence_of :managed_twitter_account
   validates_uniqueness_of :twitter_id
+
+  def url
+    "https://twitter.com/statuses/#{self.twitter_id}"
+  end
 end
