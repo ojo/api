b2eca558a62a3ce6225e9799c281bb45c13c6e28
Brian Tiger Chow <brian.holderchow@gmail.com>
2016-05-22 09:50:16 -0700

    fix(job): be conservative

--- app/jobs/fetch_tweet_embeds_job.rb
+++ app/jobs/fetch_tweet_embeds_job.rb
@@ -1,15 +1,17 @@
 class FetchTweetEmbedsJob < ApplicationJob
   queue_as :default
+  NUM_EMBEDS_PER_ACCOUNT = 10
 
   def perform(*args)
-    t = Tweet.where(embed: nil).order(twitter_id: :desc).first
-    fetch_and_save_embed t
+    ManagedTwitterAccount.all.each do |a|
+      tweets = a.tweets.order(twitter_id: :desc).take(NUM_EMBEDS_PER_ACCOUNT).keep_if { |x| x.embed == nil }
+      tweets.each { |t| fetch_and_save_embed t }
+    end
   end
 
   def fetch_and_save_embed t
-    url = "https://twitter.com/statuses/#{t.twitter_id}"
     begin
-      resp = $twitter.oembed url
+      resp = $twitter.oembed t.url
     rescue Twitter::Error::TooManyRequests 
       return
     end
