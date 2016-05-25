cf5b17f922def02f8ac9b3b851ce6d636dfe25fa
Brian Tiger Chow <brian.holderchow@gmail.com>
2016-05-24 23:41:30 -0700

    Update admin_helper.rb

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -1,10 +1,2 @@
 module AdminHelper
-  def navigation_elements
-    [
-      ['home', 'Home', admin_path],
-      ['mood', 'Social Media', admin_socialmedia_path],
-      ['date_range', 'Schedule', root_path],
-      ['fingerprint', 'Admins', admin_users_path],
-    ]
-  end
 end
