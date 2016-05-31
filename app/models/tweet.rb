17face5aef835bc742a3749466d21b9b2e61e647
Brian Tiger Chow <brian.holderchow@gmail.com>
2016-05-31 09:27:46 -0700

    provide accessor for posted at time

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -3,6 +3,13 @@ class Tweet < ApplicationRecord
   validates_presence_of :managed_twitter_account
   validates_uniqueness_of :twitter_id
 
+  # we use posted_at (instead of the DB's created_at field) because it isn't
+  # relevant when we created the tweet record locally. what matters is when the
+  # tweet was originally posted/published.
+  def posted_at
+    @posted_at ||= Time.parse(JSON.load(self.data)['created_at'])
+  end
+
   def url
     "https://twitter.com/statuses/#{self.twitter_id}"
   end
