bbcae8fa7426967a90b9e6c91bbf117cdf6b5d38
Brian Tiger Chow <brian.holderchow@gmail.com>
2016-06-01 14:39:29 -0700

    update cap

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,6 +4,8 @@ lock '3.5.0'
 set :application, 'ttrn'
 set :repo_url, 'git@github.com:briantigerchow/ttrn.git'
 
+set :rollbar_token, '5231fc821d444fe684773d02a40c3d4f'
+
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
