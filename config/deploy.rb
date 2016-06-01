9bcf071539c9ad879e6faa38aa5054c350942078
Brian Tiger Chow <brian.holderchow@gmail.com>
2016-05-31 23:26:26 -0700

    add config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,24 +1,14 @@
 # config valid only for current version of Capistrano
 lock '3.5.0'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'ttrn'
+set :repo_url, 'git@github.com:briantigerchow/ttrn.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
 # set :deploy_to, '/var/www/my_app_name'
-
-# Default value for :scm is :git
-# set :scm, :git
-
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
-
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: 'log/capistrano.log', color: :auto, truncate: :auto
 
 # Default value for :pty is false
 # set :pty, true
@@ -32,8 +22,7 @@ set :repo_url, 'git@example.com:me/my_repo.git'
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
-# Default value for keep_releases is 5
-# set :keep_releases, 5
+set :keep_releases, 10 # Default value for keep_releases is 5
 
 namespace :deploy do
 
