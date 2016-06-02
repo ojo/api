faded446042746b69c05fb13b6e8b8cc7f9d9c9f
Brian Tiger Chow <brian.holderchow@gmail.com>
2016-06-02 10:50:36 -0700

    add mailer configuration

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => 'ttrn-transactionals',
+  :password => ENV.fetch('SENDGRID_PASSWORD'),
+  :domain => 'ttrn.org',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
