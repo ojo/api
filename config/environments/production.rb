7013d7b86e2c941f4c0af4c6395d0270126b58e8
Brian Tiger Chow <brian.holderchow@gmail.com>
2016-06-02 16:42:23 -0700

    Revert "prod mail settings"
    This reverts commit bbc93950ffaf2ff5a2d67b49d27455f56bf20066.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,9 +1,5 @@
 Rails.application.configure do
   config.action_mailer.default_url_options = { host: ENV.fetch('MAILER_HOST') }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default :charset => "utf-8"
 
   # Settings specified here will take precedence over those in config/application.rb.
 
