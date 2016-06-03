0055096702bfa435a87b0b173e17120accb4da3e
Brian Tiger Chow <brian.holderchow@gmail.com>
2016-06-02 20:05:09 -0700

    use ENV.fetch wherever possible
    to fail fast

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,5 +1,5 @@
 
 $twitter = Twitter::REST::Client.new do |config|
-  config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
-  config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
+  config.consumer_key        = ENV.fetch("TWITTER_CONSUMER_KEY")
+  config.consumer_secret     = ENV.fetch("TWITTER_CONSUMER_SECRET")
 end
