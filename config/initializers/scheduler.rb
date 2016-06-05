32bb02b08d6aa27cb9001b717d2cb8cd74423e99
Brian Tiger Chow <brian.holderchow@gmail.com>
2016-06-05 09:42:04 -0700

    display recurring jobs in sidekiq portal

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -1,5 +1,8 @@
 require 'sidekiq/scheduler'
 
+require 'sidekiq/web'
+require 'sidekiq-scheduler/web'
+
 Sidekiq.configure_server do |config|
   config.on(:startup) do
     Sidekiq.schedule = YAML.load_file(File.expand_path("../../../config/scheduler.yml",__FILE__))
