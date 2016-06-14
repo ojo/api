f5a15d247ec9c8df09e5cc7bf88fd432cad42949
Brian Tiger Chow <brian.holderchow@gmail.com>
2016-06-13 20:52:27 -0700

    refactor(admin/socialmedia) form for published toggle

--- app/helpers/admin/socialmedia_helper.rb
+++ app/helpers/admin/socialmedia_helper.rb
@@ -1,4 +1,14 @@
 module Admin::SocialmediaHelper
+
+  def form_for_published_toggle entity
+    model = entity.is_a?(Array) ? entity.last : entity
+    param = :published
+    if model.send(param)
+      return form_for_toggle entity, param, false, 'Unpublish'
+    end
+    form_for_toggle entity, param, true, 'Publish'
+  end
+
   def form_for_toggle model, parameter, value, message
     class_for_value = value ? 'btn-info' : 'btn-success'
     capture do
