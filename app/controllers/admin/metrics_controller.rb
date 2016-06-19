bbc4ca19eedb4ae0f30bc869b1ddb7e923b17e4c
Brian Tiger Chow <brian.holderchow@gmail.com>
2016-06-19 02:38:41 -0700

    sort names

--- app/controllers/admin/metrics_controller.rb
+++ app/controllers/admin/metrics_controller.rb
@@ -2,11 +2,11 @@ require 'maybe' # used in view
 
 class Admin::MetricsController < Admin::BaseController
   def ttt
-    @stream_metric_names = StreamMetric.names
+    @stream_metric_names = StreamMetric.names.sort
     @time = Time.now.in_time_zone
   end
 
   def peak_times
-    @stream_metric_names = StreamMetric.names
+    @stream_metric_names = StreamMetric.names.sort
   end
 end
