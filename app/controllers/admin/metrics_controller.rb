bbc61639e24033b4019bb2e684aead325af17ef5
Brian Tiger Chow <brian.holderchow@gmail.com>
2016-06-19 02:33:47 -0700

    convert time to zone

--- app/controllers/admin/metrics_controller.rb
+++ app/controllers/admin/metrics_controller.rb
@@ -3,7 +3,7 @@ require 'maybe' # used in view
 class Admin::MetricsController < Admin::BaseController
   def ttt
     @stream_metric_names = StreamMetric.names
-    @time = Time.now
+    @time = Time.now.in_time_zone
   end
 
   def peak_times
