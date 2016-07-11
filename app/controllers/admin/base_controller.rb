4fae94de68156ef4b5da723005aaf9f2dcfe6460
Brian Tiger Chow <brian.holderchow@gmail.com>
2016-07-11 14:05:01 -0400

    move auth to admin base controller

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,3 +1,19 @@
 class Admin::BaseController < ApplicationController
   layout "bootstrap"
+
+  before_action :authenticate_user!
+
+  before_action do
+    if current_user && current_user.is_admin?
+      Rack::MiniProfiler.authorize_request
+    end
+  end
+
+  def after_sign_in_path_for(resource)
+    admin_path
+  end
+
+  def after_sign_out_path_for(resource)
+    admin_path
+  end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,22 +6,6 @@ class ApplicationController < ActionController::Base
   # TODO(btc): explore API implications
   protect_from_forgery with: :exception
 
-  before_action :authenticate_user!
-
-  before_action do 
-    if current_user && current_user.is_admin?
-      Rack::MiniProfiler.authorize_request
-    end
-  end
-
-  def after_sign_in_path_for(resource)
-    admin_path
-  end
-
-  def after_sign_out_path_for(resource)
-    admin_path
-  end
-
   def layout_by_resource
     if devise_controller?
       'bootstrap'
