2a75d2ed0f3c90a57da7468d0b07aa796994dea9
Brian Tiger Chow <brian.holderchow@gmail.com>
2016-07-11 14:04:14 -0400

    define frontend util

---
+++ lib/frontend_util.rb
@@ -0,0 +1,37 @@
+# for config/routes.rb
+module ActionDispatch
+  module Routing
+    class Mapper
+      def mount_frontends
+        FrontendUtil::FRONTENDS.each do |host|
+          reversed = FrontendUtil::to_reversed_sym host
+          constraints DomainConstraint.new(["#{host}.dev", host]) do
+            mount_ember_app reversed, to: '/'
+          end
+        end
+      end
+    end
+  end
+end
+
+class FrontendUtil
+  FRONTENDS = %w(
+    www.96wefm.com
+    www.ojo.world
+    www.1077musicforlife.com
+    www.star947tt.com
+    www.ttrn.org
+  )
+
+  # for EmberCli initializer
+  def self.config_ember_app ember_cli_config
+    FRONTENDS.each do |d|
+      reversed = self.to_reversed_sym d
+      ember_cli_config.app reversed, path: Rails.root.join('frontend', reversed)
+    end
+  end
+
+  def self.to_reversed_sym host
+    host.split('.').reverse.join('_')
+  end
+end
