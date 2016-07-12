bbc542494635380bee7875942082f88cb6aebbe5
Brian Tiger Chow <brian.holderchow@gmail.com>
2016-07-12 16:05:07 -0400

    add script to upload env vars to default environment

---
+++ .elasticbeanstalk/setenv.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+import sys
+import string
+from subprocess import call
+
+# input is 'FOO="BAR"\n'
+def transform_str_pair(pair):
+    return pair.strip('\n').replace('"', '')
+
+
+if __name__ == "__main__":
+    dotenv_filename = sys.argv[1]
+    with open(dotenv_filename) as f:
+        strs = map(transform_str_pair, f.readlines())
+        env_vars = string.join(strs)
+        cmd = ["eb", "setenv"] + strs
+        print cmd
+        call(cmd)
+
