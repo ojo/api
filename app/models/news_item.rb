ede6f38bcb768d12f41e7727a487cb9b5901b36f
Brian Tiger Chow <brian.holderchow@gmail.com>
2016-07-13 13:38:30 -0400

    fix(news_item): crop photo centrally

--- app/models/news_item.rb
+++ app/models/news_item.rb
@@ -1,4 +1,4 @@
 class NewsItem < ApplicationRecord
-  has_attached_file :photo, styles: { thumb: '100x100>' }
+  has_attached_file :photo, styles: { thumb: '100x100#' }
   validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/
 end
