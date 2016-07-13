9ae44fdad10d8a11259d14e0ddf2c7324f2b8a7a
Brian Tiger Chow <brian.holderchow@gmail.com>
2016-07-13 16:30:48 -0400

    feat(news/photos): use content addressed url

--- app/models/news_item.rb
+++ app/models/news_item.rb
@@ -1,7 +1,12 @@
 class NewsItem < ApplicationRecord
-  has_attached_file :photo, styles: {
-    thumb: { geometry: '100x100#', processors: [:papercrop] },
-    large: { geometry: '800x800#', processors: [:papercrop] },
+  has_attached_file :photo, {
+    preserve_files: true,
+    url: ':hash.:extension',
+    hash_secret: ENV.fetch('SECRET_KEY_BASE'),
+    styles: {
+      thumb: { geometry: '100x100#' },
+      large: { geometry: '800x800#' },
+    }
   }
   crop_attached_file :photo, aspect: '1:1'
   validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/
