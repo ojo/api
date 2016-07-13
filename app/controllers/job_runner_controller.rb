bbcfe2f079163b1070f95f32f47c5fcf205577b5
Brian Tiger Chow <brian.holderchow@gmail.com>
2016-07-13 10:40:56 -0400

    skip csrf check

--- app/controllers/job_runner_controller.rb
+++ app/controllers/job_runner_controller.rb
@@ -1,4 +1,6 @@
 class JobRunnerController < ApplicationController
+  protect_from_forgery :except => :perform_now
+
   def perform_now
 
     # job_name e.g. 'fetch_stream_metrics' for # FetchStreamMetricsJob
