f63df39e3fb60cc9bc7c04226b8a49e7778be3f5
Brian Tiger Chow <brian.holderchow@gmail.com>
2016-07-18 15:03:02 -0400

    validate presence of fields

--- app/models/news_item.rb
+++ app/models/news_item.rb
@@ -10,4 +10,6 @@ class NewsItem < ApplicationRecord
   }
   crop_attached_file :photo, aspect: '1:1'
   validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/
+
+  validates_presence_of :title, :body, :photo
 end
