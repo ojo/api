a90d691600de16cbc27b4c788a43504147fcb23a
Brian Tiger Chow <brian.holderchow@gmail.com>
2016-07-19 22:54:40 -0400

    more likes per second

--- app/jobs/broadcast_likes_job.rb
+++ app/jobs/broadcast_likes_job.rb
@@ -1,7 +1,7 @@
 class BroadcastLikesJob < ApplicationJob
   def perform sender_id
     r = Ratelimit.new(self.class.name, redis: $redis, bucket_interval: 1)
-    if r.exceeded?('likes', threshold: 2, interval: 1) # 2 per second
+    if r.exceeded?('likes', threshold: 10, interval: 1) # 2 per second
       return
     end
     r.add('likes')
