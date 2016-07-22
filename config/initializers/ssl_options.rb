d978ba8628d1389554a21a6e81f6fbd7053f80b8
Brian Tiger Chow <brian.holderchow@gmail.com>
2016-07-22 13:17:37 -0400

    fix: don't overwrite, merge!
    workaround

--- config/initializers/ssl_options.rb
+++ config/initializers/ssl_options.rb
@@ -1,4 +1,5 @@
 # Be sure to restart your server when you modify this file.
 
 # Configure SSL options to enable HSTS with subdomains.
-Rails.application.config.ssl_options = { hsts: { subdomains: true } }
+# FIXME: see https://github.com/rails/rails/issues/25918
+Rails.application.config.ssl_options.deep_merge! hsts: { subdomains: true }
