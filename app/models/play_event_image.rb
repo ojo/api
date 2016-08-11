8aa5e3fa5af7f38e79c93ade39ff927f90560915
Brian Tiger Chow <brian.holderchow@gmail.com>
2016-08-10 23:03:12 -0400

    add large

--- app/models/play_event_image.rb
+++ app/models/play_event_image.rb
@@ -1,7 +1,7 @@
 class PlayEventImage < ApplicationRecord
 
   # paperclip
-  has_attached_file :file, styles: { medium: "300x300>", thumb: "100x100>", mini: "50x50>" }
+  has_attached_file :file, styles: { large: "500x500>", medium: "300x300>", thumb: "100x100>", mini: "50x50>" }
   validates_attachment_content_type :file, content_type: [/\Aimage\/.*\Z/]
   before_post_process on: :create do
     if file_content_type == 'application/octet-stream'
