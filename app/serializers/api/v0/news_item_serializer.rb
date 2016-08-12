229d2a3f02e1d0e6439c6fad393ea6af10d44b0f
Brian Tiger Chow <brian.holderchow@gmail.com>
2016-08-11 22:03:04 -0400

    add category to serializer

--- app/serializers/api/v0/news_item_serializer.rb
+++ app/serializers/api/v0/news_item_serializer.rb
@@ -1,7 +1,7 @@
 class Api::V0::NewsItemSerializer < ActiveModel::Serializer
   cache key: 'news_item'
   attributes :id, :title, :subtitle, :body, :created_at, :photo_thumb_url,
-    :photo_url, :straphead, :photo_caption
+    :photo_url, :straphead, :photo_caption, :category
 
   def photo_thumb_url
     object.photo.url(:thumb)
