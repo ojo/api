85d2c728d8c19b588d3077245b86889293aaf4a8
Brian Tiger Chow <brian.holderchow@gmail.com>
2016-08-13 01:00:16 -0400

    show metrics!

--- app/controllers/admin/metrics_controller.rb
+++ app/controllers/admin/metrics_controller.rb
@@ -2,14 +2,17 @@ require 'maybe' # used in view
 
 class Admin::MetricsController < Admin::BaseController
   def index
+    authorize! :read, StreamMetric
   end
 
   def ttt
+    authorize! :read, StreamMetric
     @stream_metric_names = StreamMetric.names.sort
     @time = Time.now.in_time_zone
   end
 
   def peak_times
+    authorize! :read, StreamMetric
     @stream_metric_names = StreamMetric.names.sort
   end
 end
