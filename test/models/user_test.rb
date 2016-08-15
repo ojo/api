713fea87ad898d01173e76de2fbf1950d61dc880
Brian Tiger Chow <brian.holderchow@gmail.com>
2016-08-15 01:52:57 -0400

    clean up tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   test "create user" do
-    u = User.new email: 'me@example.com', password: 'foobar'
+    u = User.new email: 'me@example.com', password: 'foobar', full_name: 'someone new'
     u.save!
   end
 end
