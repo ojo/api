c0d8bc2b631df61da63602bba9a5ea0d26418146
Brian Tiger Chow <brian.holderchow@gmail.com>
2016-08-14 23:53:45 -0400

    include station tag in email subject

--- app/mailers/admin/play_event_image_not_found_mailer.rb
+++ app/mailers/admin/play_event_image_not_found_mailer.rb
@@ -3,7 +3,7 @@ class Admin::PlayEventImageNotFoundMailer < ApplicationMailer
     @play_event = h[:play_event]
     to = 'ojo-album-art@ttrn.org'
     from = "TTRN Admin <no-reply@ttrn.org>"
-    s = "Couldn't find album art for #{@play_event.title} by #{@play_event.artist}"
+    s = "Couldn't find album art for #{@play_event.title} by #{@play_event.artist} on #{@play_event.station.tag}"
     b = %{
 Here's the play event:
 
