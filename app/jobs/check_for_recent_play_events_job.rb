4bf5563e00746aecf29270da81aee7518baab1b4
Brian Tiger Chow <brian.holderchow@gmail.com>
2016-08-15 19:32:05 -0700

    fix: syntax error

--- app/jobs/check_for_recent_play_events_job.rb
+++ app/jobs/check_for_recent_play_events_job.rb
@@ -1,7 +1,7 @@
 class CheckForRecentPlayEventsJob < ApplicationJob
   def perform(*args)
     time_of_last_event = PlayEvent.last.created_at
-    if now - time_of_last_event > 15.minutes
+    if Time.now - time_of_last_event > 15.minutes
       Admin::NoNewPlayEventsMailer.since(time_of_last_event).deliver_now
     end
   end
